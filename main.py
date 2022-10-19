7e1ff9a8eeeffe9acc7cba82f475fd13515b38b4
computernerdzach <wizteczach@gmail.com>
2022-10-19 01:01:29 -0700

    color and reformat code

--- main.py
+++ main.py
@@ -10,15 +10,23 @@ class MyLayout(BoxLayout):
     def __init__(self):
         super().__init__()
 
-        self.button = Button(text='Two Words')
-        self.button.bind(on_press=self.get_words)
+        self.button = Button(text='Two Words',
+                             background_normal='',
+                             background_color="#0096FF")
+        self.button.bind(on_press=self.press_color)
+        self.button.bind(on_release=self.get_words)
         self.add_widget(self.button)
 
     def get_words(self, button):
+        self.button.background_color = '#0096FF'
         index1, index2 = randint(0, len(words)), randint(0, len(words))
         while index1 == index2:
             index2 = randint(0, len(words))
         self.button.text = f'{words[index1].upper()}       and       {words[index2].upper()}'
+
+    def press_color(self, button):
+        self.button.background_normal = ''
+        self.button.background_color = '#FA8072'
 
 
 class MyApp(App):
